a08d2de2bc7f202c93998f2fadba4bb03abf9a60
JuanShos <juanito.martinon5@gmail.com>
2026-07-15 23:15:39 -0700

    list of controller

--- KnowBeforeYouGo/ViewController.swift
+++ KnowBeforeYouGo/ViewController.swift
@@ -11,11 +11,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello world")
-        print("im alive i think")
-        print("csdcdscdjwjncdwjdw jkcwdbjcwdjcdwjndwc")
-        print("4737437843783478437843")
+        setupTabBar()
     }
-
-
+    
+    private func setupTabBar() {
+        // Create the tab bar controller
+        let tabBarController = UITabBarController()
+        
+        // Create Map view controller
+        let mapViewController = MapOfViewController()
+        mapViewController.view.backgroundColor = .systemBackground
+        mapViewController.tabBarItem = UITabBarItem(title: "Map", image: UIImage(systemName: "map"), tag: 0)
+        
+        // Create List view controller
+        let listViewController = ListOfViewController()
+        listViewController.view.backgroundColor = .systemBackground
+        listViewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "list.bullet"), tag: 1)
+        
+        // Add view controllers to tab bar
+        tabBarController.viewControllers = [mapViewController, listViewController]
+        
+        // Add tab bar controller as a child
+        addChild(tabBarController)
+        view.addSubview(tabBarController.view)
+        tabBarController.view.frame = view.bounds
+        tabBarController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tabBarController.didMove(toParent: self)
+    }
 }
