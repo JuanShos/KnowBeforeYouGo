4ceffc4f2002af2bcb840a635458af6a2d096eb5
JuanShos <juanito.martinon5@gmail.com>
2026-07-15 23:15:16 -0700

    new view controllers and such

---
+++ KnowBeforeYouGo/ListOfViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ListOfViewController.swift
+//  KnowBeforeYouGo
+//
+//  Created by Juanito Martinon on 7/15/26.
+//
+
+import UIKit
+
+class ListOfViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
