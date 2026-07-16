c7d023a152c00de44b22f29bd6ec5c369967aa02
JuanShos <juanito.martinon5@gmail.com>
2026-07-15 20:55:21 -0700

    Initial Commit

---
+++ KnowBeforeYouGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KnowBeforeYouGo
+//
+//  Created by Juanito Martinon on 7/15/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello world")
+        print("im alive i think")
+    }
+
+
+}
+
---
+++ KnowBeforeYouGoTests/KnowBeforeYouGoTests.swift
@@ -0,0 +1,19 @@
+//
+//  KnowBeforeYouGoTests.swift
+//  KnowBeforeYouGoTests
+//
+//  Created by Juanito Martinon on 7/15/26.
+//
+
+import Testing
+@testable import KnowBeforeYouGo
+
+struct KnowBeforeYouGoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
